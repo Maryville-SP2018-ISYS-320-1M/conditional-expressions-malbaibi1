59d0d4801577dcefa066ed3944ea975b9a51b5e9
malbaibi1 <31707529+malbaibi1@users.noreply.github.com>
2018-04-20 22:36:34 +0500

    Updated

--- src/P1_BooleanExpressions.java
+++ src/P1_BooleanExpressions.java
@@ -1,47 +1,53 @@
 /*
-	ISYS 320
-	Name(s):
-	Date: 
+ ISYS 320
+ Name: Mansour Albaibi
+ Date: 21-4-2018 
 */
 
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. True
+ 2. True
+ 3. False
+ 4. True
  
- 5.
- 6.
- 7.
- 8.
+ 5. True
+ 6. False
+ 7. True
+ 8. True
    
  */
 
 /*
  Were you correct? Explain if you were not (what did you learn?)
  
- 1.
- 2.
- 3. 
- 4.
+ 1. Correct
+ 2. Correct
+ 3. Correct
+ 4. Correct
  
- 5.
- 6.
- 7.
- 8.
-
+ 5. Correct
+ 6. Correct
+ 7. Correct
+ 8. Correct
  */
 public class P1_BooleanExpressions {
 
-	public static void main(String[] args) {
-		int x = 27; 
-		int y = -1; 
-		int z = 32; 
-		boolean b = false; 
-		
-		System.out.println("REPLACE THIS STRING WITH YOUR BOOLEAN EXPRESSION");
-	}
+ public static void main(String[] args) {
+  int x = 27; 
+  int y = -1; 
+  int z = 32; 
+  boolean b = false; 
+  
+  System.out.println("1. " + !b);
+  System.out.println("2. " + (b || true));
+  System.out.println("3. " + ((x > y)&&(y > z)));
+  System.out.println("4. " + ((x == y)||( x <= z)));
+  System.out.println("5. " + (!(x % 2 == 0)));
+  System.out.println("6. " + (b && !b));
+  System.out.println("7. " + (b || !b));
+  System.out.println("8. " + ((x < y) == b));
+ }
 
 }
--- src/P3_Consecutive.java
+++ src/P3_Consecutive.java
@@ -1,38 +1,55 @@
 /*
-	ISYS 320
-	Name(s):
-	Date: 
+ ISYS 320
+ Name: Mansour Albaibi
+ Date: 21-4-2018 
 */
 
+import java.lang.*;
 import java.util.Scanner;
 
 public class P3_Consecutive {
 
-	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
-		System.out.println("This program calculates if three input values are consecutive.");
-		
-		String doItAgainResponse = "";
-		
-		do {
-			System.out.print("Enter value 1: ");
-			int num1 = console.nextInt();
-			System.out.print("Enter value 2: ");
-			int num2 = console.nextInt();
-			System.out.print("Enter value 3: ");
-			int num3 = console.nextInt();
-		
-			System.out.printf( "a: %d, b: %d, c: %d -> ", num1, num2, num3);
-			
+ public static void main(String[] args) {
+  Scanner console = new Scanner(System.in);
+  System.out.println("This program calculates if three input values are consecutive.");
+  
+  String doItAgainResponse = "";
+  
+  do {
+   System.out.print("Enter value 1: ");
+   int num1 = console.nextInt();
+   System.out.print("Enter value 2: ");
+   int num2 = console.nextInt();
+   System.out.print("Enter value 3: ");
+   int num3 = console.nextInt();
+  
+   System.out.printf( "a: %d, b: %d, c: %d -> ", num1, num2, num3);
+   boolean numb = areConsectiveNumbers(num1,num2,num3);
+   if (numb)
+     System.out.println("They are consective.");
+   else
+     System.out.println("They are not consective.");
+     
+   
 
-			
-			System.out.print("Would you like to do it again (enter 'n' to quit)? ");
-			doItAgainResponse = console.next();
-		} while( !doItAgainResponse.equalsIgnoreCase("n"));
-		
-		System.out.println("Thanks!");
-	}
-	
+   
+   System.out.print("Would you like to do it again (enter 'n' to quit)? ");
+   doItAgainResponse = console.next();
+  } while( !doItAgainResponse.equalsIgnoreCase("n"));
+  
+  System.out.println("Thanks!");
+ }
+  public static boolean areConsectiveNumbers(int a, int b, int c) {
+    boolean num = false;
+    int x = Math.abs(a);
+    int y = Math.abs(b);
+    int z = Math.abs(c);
+    if((x-y==1)||(y-x==1)||(z-x==1)||(x-z==1))
+      if((y-z==1)||(z-y==1)||(y-x==1)||(x-y==1))
+      num =true;
+
+  return num;
+  }
 
 
 }
